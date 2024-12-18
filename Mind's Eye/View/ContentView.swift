fbcdd62f22ea726539e76918356fc500d0f50b78
itsBashayer <alfuhaidbashayer@gmail.com>
2024-12-18 12:14:54 +0300

    pp

---
+++ Mind's Eye/View/ContentView.swift
@@ -0,0 +1,11 @@
+
+//import SwiftUI
+//
+//@main
+//struct MindsEyeApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            MindEyeViewModel() // Starting point of your app
+//        }
+//    }
+//}
