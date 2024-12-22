03060031d853068f08243399ac95fd44372fc179
Sumayahalsh <somayahalsh321@gmail.com>
2024-12-22 11:16:17 +0300

    cards

--- Mind's Eye/View/Cards.swift
+++ Mind's Eye/View/Cards.swift
@@ -1,95 +1,165 @@
 import SwiftUI
 
 struct Cards: View {
-    @State private var userAnswers = [String]() // قائمة لتخزين إجابات المستخدم
+    @State private var userAnswers = [String]()
     @State private var showSuccessPopup = false
-    @State private var score = 0 // متغير لحساب النقاط
+    @State private var score = 0
+    @State private var evidences = ["Key", "Wound"].shuffled()
+    @State private var navigateToMainMenu = false
+    @State private var currentInput = "" // النص المدخل في TextField
 
     let correctAnswers = ["key", "wound"]
-    
-    // ترتيب عشوائي للكروت
-    @State private var evidences = ["Key", "Wound"].shuffled()
 
     var body: some View {
-        ZStack {
-            Image("background")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
+        NavigationStack {
+            ZStack {
+                // Background image
+                Image("background")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
 
-            VStack {
-                Text("Collect Evidence")
-                    .font(.custom("Questv1-Bold", size: 32))
-                    .foregroundColor(.white)
-                    .padding(.bottom, 40)
-                    .shadow(color: .white, radius: 10, x: 0, y: 0)
-                
-                HStack(spacing: 20) {
-                    ForEach(evidences, id: \.self) { evidence in
-                        EvidenceCard(evidence: evidence, isCorrect: userAnswers.contains(evidence.lowercased()))
-                    }
-                }
-                
-                VStack(spacing: 10) {
+                VStack {
+                    // Score bar
                     HStack {
-                        Image(systemName: "mic.fill")
-                            .foregroundColor(.white)
-                            .padding(.leading, 10)
-                        
-                        TextField("Enter your answer", text: Binding(
-                            get: { "" },
-                            set: { newValue in
-                                let answer = newValue.lowercased()
-                                if correctAnswers.contains(answer) && !userAnswers.contains(answer) {
-                                    userAnswers.append(answer)
-                                    score = min(userAnswers.count, 3) // حساب النقاط بحد أقصى 3
-                                }
-                                if userAnswers.count == correctAnswers.count {
-                                    showSuccessPopup = true
-                                }
-                            }))
-                            .foregroundColor(.white)
-                            .padding(.vertical, 10)
-                            .padding(.horizontal, 5)
+                        Text("\(score) / \(correctAnswers.count)")
+                            .font(.custom("Questv1-Bold", size: 24))
+                            .foregroundColor(.red)
+                            .padding(8)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color.white.opacity(0.8))
+                                    .shadow(color: Color.red.opacity(0.6), radius: 4)
+                            )
+                            .padding(.leading)
+                            .padding(.top, 40)
+                        Spacer()
                     }
-                    .background(Color(red: 0.1, green: 0, blue: 0))
-                    .cornerRadius(25)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 25)
-                            .stroke(Color.white, lineWidth: 2)
-                    )
-                    .frame(width: 300)
-                    
-                    Button("Send") {
-                        if userAnswers.count == correctAnswers.count {
-                            showSuccessPopup = true
+                    Spacer()
+                }
+
+                VStack {
+                    Text("Collect Evidence")
+                        .font(.custom("Questv1-Bold", size: 32))
+                        .foregroundColor(.white)
+                        .padding(.bottom, 40)
+                        .shadow(color: .white, radius: 10, x: 0, y: 0)
+
+                    HStack(spacing: 20) {
+                        ForEach(evidences, id: \.self) { evidence in
+                            EvidenceCard(
+                                evidence: evidence,
+                                isCorrect: userAnswers.contains(evidence.lowercased()),
+                                onFlip: { flippedEvidence in
+                                    if correctAnswers.contains(flippedEvidence.lowercased()) && !userAnswers.contains(flippedEvidence.lowercased()) {
+                                        userAnswers.append(flippedEvidence.lowercased())
+                                        score += 1
+                                    }
+                                    if userAnswers.count == correctAnswers.count {
+                                        showSuccessPopup = true
+                                    }
+                                }
+                            )
                         }
                     }
-                    .font(.custom("Questv1-Bold", size: 20))
-                    .foregroundColor(.white)
-                    .padding(.vertical, 5)
+
+                    VStack(spacing: 10) {
+                        HStack {
+                            Image(systemName: "mic.fill")
+                                .foregroundColor(.white)
+                                .padding(.leading, 10)
+
+                            TextField("Enter your answer", text: $currentInput)
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 5)
+                        }
+                        .background(Color(red: 0.1, green: 0, blue: 0))
+                        .cornerRadius(25)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(Color.white, lineWidth: 2)
+                        )
+                        .frame(width: 300)
+
+                        Button("Send") {
+                            let answer = currentInput.lowercased()
+                            if correctAnswers.contains(answer) && !userAnswers.contains(answer) {
+                                userAnswers.append(answer)
+                                score += 1
+                                currentInput = "" // إعادة تعيين النص
+                            }
+                            if userAnswers.count == correctAnswers.count {
+                                showSuccessPopup = true
+                            }
+                        }
+                        .font(.custom("Questv1-Bold", size: 20))
+                        .foregroundColor(.white)
+                        .padding(.vertical, 5)
+                        .disabled(currentInput.isEmpty) // تعطيل الزر إذا كان النص فارغًا
+                        .opacity(currentInput.isEmpty ? 0.5 : 1.0) // تغيير شفافية الزر
+                    }
+                    .padding()
                 }
-                .padding()
+
+                if showSuccessPopup {
+                    Color.black.opacity(0.5)
+                        .ignoresSafeArea()
+                        .transition(.opacity)
+
+                    VStack(spacing: 20) {
+                        Text("⭐ Score: \(score) / \(correctAnswers.count)")
+                            .font(.custom("Questv1-Bold", size: 24))
+                            .foregroundColor(.yellow)
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .cornerRadius(20)
+
+                        CustomPopup(showSuccessPopup: $showSuccessPopup)
+
+                        VStack(spacing: 20) {
+                            Button("Go to the Next Level") {
+                                withAnimation {
+                                    userAnswers.removeAll()
+                                    score = 0
+                                    evidences.shuffle()
+                                    showSuccessPopup = false
+                                }
+                            }
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.red)
+                            .padding()
+                            .frame(width: 350, height: 52)
+                            .background(Color.white)
+                            .cornerRadius(30)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 30)
+                                    .stroke(Color.red, lineWidth: 2)
+                            )
+
+                            // Navigation to Main Menu
+                            Button("Return to the Main Menu") {
+                                navigateToMainMenu = true
+                            }
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .padding()
+                            .frame(width: 350, height: 52)
+                            .background(Color.black)
+                            .cornerRadius(30)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 30)
+                                    .stroke(Color.white, lineWidth: 2)
+                            )
+                        }
+                        .padding(.top, 40)
+                    }
+                }
             }
-
-            // تأثير الضبابية الداكنة عند ظهور الـ popup
-            if showSuccessPopup {
-                Color.black.opacity(0.5)
-                    .ignoresSafeArea()
-                    .transition(.opacity)
-
-                VStack(spacing: 20) {
-                    // شريط النقاط (streak) فوق البوب اب
-                    Text("⭐ Score: \(score) / 3")
-                        .font(.custom("Questv1-Bold", size: 24))
-                        .foregroundColor(.yellow)
-                        .padding()
-                        .background(Color.black.opacity(0.7))
-                        .cornerRadius(20)
-
-                    CustomPopup(showSuccessPopup: $showSuccessPopup)
-                    PopupButtons(showSuccessPopup: $showSuccessPopup)
-                }
+            .navigationDestination(isPresented: $navigateToMainMenu) {
+                CaseSelectionView()
             }
         }
         .animation(.easeInOut, value: showSuccessPopup)
@@ -99,16 +169,15 @@ struct Cards: View {
 struct EvidenceCard: View {
     let evidence: String
     let isCorrect: Bool
+    let onFlip: (String) -> Void
 
     @State private var flipped = false
-    @State private var rotation = 180.0 // تبدأ مقلوبة
-    @State private var shakeOffset: CGFloat = 0 // متغير للاهتزاز
+    @State private var rotation = 180.0
 
     var body: some View {
         ZStack {
             Group {
                 if flipped {
-                    // الوجه الأمامي - يظهر بعد الإجابة الصحيحة
                     Text(evidence)
                         .font(.custom("Questv1-Bold", size: 24))
                         .frame(width: 150, height: 200)
@@ -116,7 +185,6 @@ struct EvidenceCard: View {
                         .cornerRadius(20)
                         .foregroundColor(.white)
                 } else {
-                    // الوجه الخلفي - يظهر في البداية
                     RoundedRectangle(cornerRadius: 20)
                         .fill(Color.black)
                         .frame(width: 150, height: 200)
@@ -129,12 +197,10 @@ struct EvidenceCard: View {
             }
             .rotation3DEffect(.degrees(rotation), axis: (x: 0, y: 1, z: 0))
         }
-        .offset(x: shakeOffset) // تطبيق تأثير الاهتزاز
         .onTapGesture {
-            if isCorrect {
+            if !flipped {
                 flipCard()
-            } else {
-                shakeCard() // اهتزاز عند الإجابة الخاطئة
+                onFlip(evidence)
             }
         }
     }
@@ -143,28 +209,6 @@ struct EvidenceCard: View {
         withAnimation(.easeInOut(duration: 0.6)) {
             rotation += 180
             flipped.toggle()
-        }
-    }
-
-    func shakeCard() {
-        let shakeAnimation = Animation.interpolatingSpring(stiffness: 100, damping: 5)
-        withAnimation(shakeAnimation) {
-            shakeOffset = 15
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-            withAnimation(shakeAnimation) {
-                shakeOffset = -15
-            }
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            withAnimation(shakeAnimation) {
-                shakeOffset = 10
-            }
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-            withAnimation(shakeAnimation) {
-                shakeOffset = 0
-            }
         }
     }
 }
@@ -199,48 +243,7 @@ struct CustomPopup: View {
     }
 }
 
-struct PopupButtons: View {
-    @Binding var showSuccessPopup: Bool
 
-    var body: some View {
-        VStack(spacing: 20) {
-            Button("Go to the Next Level") {
-                withAnimation {
-                    showSuccessPopup = false
-                }
-            }
-            .font(.headline)
-            .fontWeight(.bold)
-            .foregroundColor(Color.red)
-            .padding()
-            .frame(width: 350, height: 52)
-            .background(Color.white)
-            .cornerRadius(30)
-            .overlay(
-                RoundedRectangle(cornerRadius: 30)
-                    .stroke(Color.red, lineWidth: 2)
-            )
-
-            Button("Return to the Main Menu") {
-                withAnimation {
-                    showSuccessPopup = false
-                }
-            }
-            .font(.headline)
-            .fontWeight(.bold)
-            .foregroundColor(Color.white)
-            .padding()
-            .frame(width: 350, height: 52)
-            .background(Color.black)
-            .cornerRadius(30)
-            .overlay(
-                RoundedRectangle(cornerRadius: 30)
-                    .stroke(Color.white, lineWidth: 2)
-            )
-        }
-        .padding(.top, 40)
-    }
-}
 
 struct Cards_Previews: PreviewProvider {
     static var previews: some View {
